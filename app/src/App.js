2e311ba23e205ca7c733d2c4ade582521ec45f27
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-10-07 20:44:40 +0300

    Убейте меня

---
+++ app/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './app.css';
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [books, setBooks] = useState([]);
+  
+  const API_KEY = 'AIzaSyDDSwsmp9VSXi_OkoTm6tCHH9fM3zFNBmc';
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`);
+      const data = await response.json();
+      console.log(response);
+      console.log(data);
+      setBooks(data.items || []);
+    } 
+    catch (error) {
+      console.error("Ошибка при загрузке книг:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Поиск книг</h1>
+      <input 
+        type="text" 
+        placeholder="Поиск..." 
+        value={query} 
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            fetchBooks();
+          }
+        }}
+      />
+      <div className="books">
+        {books.map((book) => (
+          <div key={book.id} className="book-card">
+            <img
+              src={book.volumeInfo.imageLinks?.thumbnail}
+              alt={book.volumeInfo.title}
+            />
+            <h2>{book.volumeInfo.title}</h2>
+            <p>{book.volumeInfo.categories ? book.volumeInfo.categories[0] : "Категория не указана"}</p>
+            <p>{book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "Авторы не указаны"}</p>
+          </div>
+        ))}
+      </div>
+      <footer>
+        <a href="https://github.com/morganizwd">GitHub</a>
+        <span>© 2023</span>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
