f2c817a662db1c32e41802cf567444dc0905fc95
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-02 23:39:48 +0300

    feat: add style for searchbar

--- lab4/src/components/SearchBar.js
+++ lab4/src/components/SearchBar.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
+import styles from './SearchBar.module.css';
 
 function SearchBar({ onSearch }) {
   const [inputValue, setInputValue] = useState('');
 
   const handleSearch = () => {
-    onSearch(inputValue);
+    onSearch(inputValue.trim());
   };
 
   return (
-    <div>
+    <div className={styles.searchBarContainer}>
       <input
+        className={styles.searchInput}
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         placeholder="Search for books"
         onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
       />
-      <button onClick={handleSearch}>Search</button>
+      <button
+        className={styles.searchButton}
+        onClick={handleSearch}
+      >
+        Search
+      </button>
     </div>
   );
 }
