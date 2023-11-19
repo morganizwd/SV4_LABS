c5319b06f6dc9440de9b5bfb115811ddad9c7196
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-19 15:58:24 +0300

    feat: create footer component

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,10 +1,12 @@
 import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ lab5/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>Свяжитесь с нами:</p>
+            <p>Email: <a href="mailto:morganizwd@gmail.com">morganizwd@gmail.com</a></p>
+            <p>Телефон: <a href="tel:+375336146223">+375336146223</a></p>
+            <p>© {new Date().getFullYear()} Все под контролем. Все права защищены.</p>
+        </footer>
+    );
+}
+
+export default Footer;
