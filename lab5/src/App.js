302d26975d41161e3891125dd32cf44f04f78329
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-19 16:14:05 +0300

    feat: add json file ad a card's data

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,11 +1,13 @@
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer';
+import HomePage from './Components/HomePage/HomaPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HomePage/>
       <Footer/>
     </div>
   );
