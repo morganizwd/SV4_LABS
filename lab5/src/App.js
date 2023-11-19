8d6c6bfa18f1a09c8cb9c15e6706154585176061
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-19 17:04:43 +0300

    fix: i fix something somehow

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,17 +1,25 @@
-import Header from './Components/Header/Header'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import HomePage from './Components/HomePage/HomaPage';
-import CertificationServicesPage from './Components/SertificationPage/SetreficationPageServices'
+import CertificationServicesPage from './Components/SertificationPage/SetreficationPageServices';
+import QualityControlPage from './Components/QualityControlPage/QualityControlPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <HomePage/>
-      <CertificationServicesPage/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/certification-services" element={<CertificationServicesPage />} />
+          <Route path="/quality-control" element={<QualityControlPage />} />
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
