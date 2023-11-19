455a54467b4f598a5a1e2eef08c7be8a70f41b3b
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-19 15:54:10 +0300

    feat: create header component

---
+++ lab5/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <div className="header">
+            <ul>
+                <li><strong>Все под контролем</strong></li>
+            </ul>
+            <ul>
+                <li><a href="#about">О нас</a></li>
+                <li><a href="#products">Продукция</a></li>
+                <li><a href="#quality-control">Контроль качества</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
