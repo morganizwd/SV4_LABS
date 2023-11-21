ca742b914ea07a0a64f76988cbc23f1f34ea8fca
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-21 15:24:35 +0300

    feat: add burger and side menu for header

--- lab5/src/Components/Header/Header.jsx
+++ lab5/src/Components/Header/Header.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './style.css'
+import './style.css';
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className="header">
             <ul className='logo'>
                 <li><strong>Все под контролем, брат</strong></li>
             </ul>
-            <ul className='content'>
-                <li><Link to="/">Главная</Link></li>
-                <li><Link to="/certification-services">Продукция</Link></li>
-                <li><Link to="/quality-control">Контроль качества</Link></li>
+            <button className="hamburger" onClick={handleMenuToggle}>
+                &#9776; {/* This is a common icon for hamburger menus */}
+            </button>
+            <ul className={`content ${isMenuOpen ? "active" : ""}`}>
+                <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Главная</Link></li>
+                <li><Link to="/certification-services" onClick={() => setIsMenuOpen(false)}>Продукция</Link></li>
+                <li><Link to="/quality-control" onClick={() => setIsMenuOpen(false)}>Контроль качества</Link></li>
             </ul>
         </div>
     );
 }
 
-export default Header;
+export default Header; 
