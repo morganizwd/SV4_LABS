d02fc335a1543cf04fa1daa61f6df765b0402433
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-21 10:59:01 +0300

    refactor: adjust classname

--- lab5/src/Components/Header/Header.jsx
+++ lab5/src/Components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './style.css'
 
 function Header() {
     return (
         <div className="header">
-            <ul>
+            <ul className='logo'>
                 <li><strong>Все под контролем, брат</strong></li>
             </ul>
-            <ul>
+            <ul className='content'>
                 <li><Link to="/">Главная</Link></li>
                 <li><Link to="/certification-services">Продукция</Link></li>
                 <li><Link to="/quality-control">Контроль качества</Link></li>
