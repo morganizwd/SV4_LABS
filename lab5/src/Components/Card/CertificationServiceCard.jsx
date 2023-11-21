3957abe7881691acc845c61b2b8b8542d13dcddf
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-21 10:33:11 +0300

    feat: modify card component for proper work with popout

--- lab5/src/Components/Card/CertificationServiceCard.jsx
+++ lab5/src/Components/Card/CertificationServiceCard.jsx
@@ -1,17 +1,24 @@
+import React, { useState } from 'react';
+import Popout from '../PopoutWindow/Popout';
+
 function CertificationServiceCard({ service }) {
+    const [isPopoutOpen, setIsPopoutOpen] = useState(false);
+
+    const togglePopout = () => {
+        setIsPopoutOpen(!isPopoutOpen);
+    };
+
     return (
-        <div className="service-card">
-            <img src={service.image} alt={service.name} className="service-image" />
-            <div className="service-content">
-                <h3 className="service-title">{service.name}</h3>
-                <p className="service-description">{service.description}</p>
-                <ul className="service-features">
-                    {service.features.map((feature, index) => (
-                        <li key={index} className="feature">{feature}</li>
-                    ))}
-                </ul>
+        <>
+            <div className="service-card" onClick={togglePopout}>
+                <img src={service.image} alt={service.name} className="service-image" />
+                <div className="service-content">
+                    <h3 className="service-title">{service.name}</h3>
+                    <p className="service-description">{service.description}</p> 
+                </div>
             </div>
-        </div>
+            {isPopoutOpen && <Popout service={service} closePopout={togglePopout} />}
+        </>
     );
 }
 
