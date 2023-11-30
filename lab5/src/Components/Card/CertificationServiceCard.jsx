310fc31d6174af4034ef1e37471a757305f9e67d
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-30 19:04:39 +0300

    refactor: implinet mui for cards

--- lab5/src/Components/Card/CertificationServiceCard.jsx
+++ lab5/src/Components/Card/CertificationServiceCard.jsx
@@ -1,31 +1,59 @@
 import React, { useState } from 'react';
+import { Card, CardContent, CardMedia, Typography, Button, CardActions } from '@mui/material';
 import Popout from '../PopoutWindow/Popout';
-import './style.css';
 
 function CertificationServiceCard({ service, isSelected, toggleSelect }) {
     const [isPopoutOpen, setIsPopoutOpen] = useState(false);
 
     const handleCardClick = () => {
         if (!isPopoutOpen) {
-            toggleSelect(service.id); // Вызов toggleSelect только если Popout не открыт
+            toggleSelect(service.id);
         }
     };
 
     const togglePopout = (e) => {
-        e.stopPropagation(); // Предотвратить вызов handleCardClick при клике на карточку
+        e.stopPropagation();
         setIsPopoutOpen(!isPopoutOpen);
+    };
+
+    const cardStyle = {
+        maxWidth: 345, // Ширина карточки
+        height: '100%', // Высота карточки
+        margin: 'auto',
+        border: isSelected ? '2px solid blue' : 'none',
+        cursor: 'pointer',
+        display: 'flex',
+        flexDirection: 'column',
+    };
+
+    const cardMediaStyle = {
+        height: 140, // Высота изображения в карточке
     };
 
     return (
         <>
-            <div className={`service-card ${isSelected ? 'selected' : ''}`} onClick={handleCardClick}>
-                <img src={service.image} alt={service.name} className="service-image" />
-                <div className="service-content">
-                    <h3 className="service-title">{service.name}</h3>
-                    <p className="service-description">{service.description}</p> 
-                    <button onClick={togglePopout}>Подробнее</button>
-                </div>
-            </div>
+            <Card 
+                onClick={handleCardClick}
+                style={cardStyle}
+            >
+                <CardMedia
+                    component="img"
+                    style={cardMediaStyle}
+                    image={service.image}
+                    alt={service.name}
+                />
+                <CardContent style={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {service.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {service.description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" onClick={togglePopout}>Подробнее</Button>
+                </CardActions>
+            </Card>
             {isPopoutOpen && <Popout service={service} closePopout={togglePopout} />}
         </>
     );
