478a0c3cc49bd0ffd97414d8ef21744b78ebe972
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-11-30 19:00:32 +0300

    refactor: impliment mui for footer

--- lab5/src/Components/Footer/Footer.jsx
+++ lab5/src/Components/Footer/Footer.jsx
@@ -1,13 +1,20 @@
-import './style.css';
+import React from 'react';
+import { Container, Typography, Link } from '@mui/material';
 
 function Footer() {
     return (
-        <footer className="footer">
-            <p>Свяжитесь с нами:</p>
-            <p>Email: <a href="mailto:morganizwd@gmail.com">morganizwd@gmail.com</a></p>
-            <p>Телефон: <a href="tel:+375336146223">+375336146223</a></p>
-            <p>© {new Date().getFullYear()} Все под контролем. Все права защищены.</p>
-        </footer>
+        <Container component="footer" style={{ padding: '20px 0', textAlign: 'center' }}>
+            <Typography variant="body1">Свяжитесь с нами:</Typography>
+            <Typography variant="body1">
+                Email: <Link href="mailto:morganizwd@gmail.com">morganizwd@gmail.com</Link>
+            </Typography>
+            <Typography variant="body1">
+                Телефон: <Link href="tel:+375336146223">+375336146223</Link>
+            </Typography>
+            <Typography variant="body1" style={{ marginTop: '15px' }}>
+                © {new Date().getFullYear()} Все под контролем. Все права защищены.
+            </Typography>
+        </Container>
     );
 }
 
